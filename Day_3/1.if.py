3481fadb519a4b988a10048df688781a71c5ddd4
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2025-07-06 15:43:53 +0530

    [Add]Day_2

---
+++ Day_3/1.if.py
@@ -0,0 +1,6 @@
+# Asking the user for their age
+age = int(input("Enter your age: "))      # Convert input to integer
+
+# Checking if age is greater than or equal to 18
+if age >= 18:
+    print("You are eligible to vote.")    # This line runs if condition is True
