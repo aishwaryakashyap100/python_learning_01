2cf03ce49e8efb3e0c0cc46da27a01201c06b1d7
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2025-07-08 20:09:51 +0530

    [Add]6.h.w.py

---
+++ Day_1/6.h.w.py
@@ -0,0 +1,23 @@
+#task 1
+Name = input("Enter your name:")
+
+physics = int(input("Enter marks of physics:"))
+chemistry = int(input("Enter marks of chemistry:"))
+mathematics = int(input("Enter marks of mathematics:"))
+
+total_marks = physics + chemistry + mathematics
+
+percentage = (total_marks/300)*100
+
+print (Name)
+print (percentage)
+
+
+#task 2
+number_1 = int(input("Enter number_1:"))
+number_2 = int(input("Enter number_2:"))
+
+print (number_1+number_2)
+print (number_1-number_2)
+print (number_1*number_2)
+print (number_1/number_2)
