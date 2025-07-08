f7e635f73da2cb01feabcdbd9e593d96129e0a02
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2025-07-08 20:27:07 +0530

    7.h.w.py

--- Day_2/7.h.w.py
+++ Day_2/7.h.w.py
@@ -12,4 +12,8 @@ print (number_1**number_2)
 
 #task 2
 age = int(input("Enter your age:"))
-ID = input("You have id or not:")
+ID = input("You have id? (yes/no):")
+if age>=18 and ID.lower() == "yes":
+     print("Entry allowed")
+else:
+     print("Entry Denied")
