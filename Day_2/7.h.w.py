d4b1a1ec73a6b875e27aa6b3eed5573a373ca3e6
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2025-07-08 20:17:43 +0530

    7.h.w.py

---
+++ Day_2/7.h.w.py
@@ -0,0 +1,15 @@
+#task 1
+number_1 = int(input("Enter number_1:"))
+number_2 = int(input("Enter number_2:"))
+print (number_1+number_2)
+print (number_1-number_2)
+print (number_1*number_2)
+print (number_1/number_2)
+print (number_1//number_2)
+print (number_1%number_2)
+print (number_1**number_2)
+
+
+#task 2
+age = int(input("Enter your age:"))
+ID = input("You have id or not:")
