553a085fbccd40cff94427b86c772104aad0ba25
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2025-07-09 18:19:26 +0530

    8.practice.py

---
+++ Day_2/8.practice.py
@@ -0,0 +1,15 @@
+# 1 
+Age = int(input("Enter your Age:"))
+
+if Age>=18:
+    print ("Eligible to vote")
+else:
+    print ("Not Eligible")
+
+# 2
+Marks = float(input("Enter your Marks:"))
+
+if Marks>=33:
+    print("Pass")
+else:
+    print("Fail")
